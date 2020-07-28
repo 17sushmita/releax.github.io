eccef7cfe2dbc25dd7ef987b06288c7a40836653
17sushmita <17sushmita@gmail.com>
2020-07-28 13:06:31 +0530

    minor changes

--- releax_jscript.js
+++ releax_jscript.js
@@ -1,8 +1,7 @@
 var tabButtons=document.querySelectorAll(".index .tabContainer .buttonContainer button");
 var tabPanels=document.querySelectorAll(".content .content1");
 
-function showPanel(panelIndex) {
-    
+function showPanel(panelIndex,btn) {
     
     tabPanels.forEach(function(node){
         node.style.display="none";
@@ -13,7 +12,7 @@ showPanel(0);
 
 
     // for subindex span button toggling effect
-
+/*
       $('.feat-btn').click(function(){
         $('ol .feat-show').toggleClass("show");
         $('ol .first').toggleClass("rotate");
@@ -21,9 +20,22 @@ showPanel(0);
       $('li').click(function(){
         $(this).addClass("active").siblings().removeClass("active");
       });
+      */
+     $('.feat-btn').click(function(){
+      $('.feat-show').animate({
+        height: "toggle"
+      },500);
+      $('ol .first').toggleClass("rotate");
+    });
+    $('li').click(function(){
+      $('li').removeClass('active');
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+    
 
 function toggleSidebar(){
     document.getElementById('index').classList.toggle('active');
     document.getElementById('content').classList.toggle('slide');
 
 }
+
